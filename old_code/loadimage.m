c2934c72682b10ad88d2595d9dbbd9f5c4bad3b9
Jeshua Maxey <me@jeshua.co>
2014-01-11 18:29:00 +0000

    removed old code scripts

--- old_code/loadimage.m
+++
@@ -1,8 +0,0 @@
-function [ image ] = loadimage()
-%LOADIMAGE Summary of this function goes here
-%   Detailed explanation goes here
-    % load in fits data
-    image = fitsread('mosaic.fits');
-
-end
-
